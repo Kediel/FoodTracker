2134d1336db6dd16a4a52188ee1f457adae154f0
KedielM <km3556@drexel.edu>
2018-02-02 15:14:35 -0500

    Created my Data Model file
    Added the initial class Meal.

---
+++ Meal.swift
@@ -0,0 +1,20 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Kediel on 2/2/18.
+//  Copyright © 2018 riOS. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // Mark: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    
+}
